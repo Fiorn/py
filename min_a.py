50bbca3dee282dbcfc6bde2067555f465a10b591
fiorn <neros10v@yandex.ru>
2021-11-02 23:32:15 +0500

    задание из презентации полностью

--- min_a.py
+++ min_a.py
@@ -1,5 +1,18 @@
-A = [3,4,2,1,0,8,9,-1,100,0]
+A = [66,3,75,7,4,2,1,-1.5,0,8,9,-1,100,0,12,4,-0.5,9]
+D = []
 min = A.pop()
-  while A:
-  a = A.pop()
-print A 
+max = A.pop()
+A.append(max)
+A.append(min)
+while A:
+    a = A.pop()
+    b = a
+    D.append(1)
+    e=D.count(1)
+    print (e,a)
+    if a < min:
+        min = a
+    if b > max:
+        max = b
+print (max)
+print (min)
