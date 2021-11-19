a7f215c9b1686da4aea7b7b61b41db23ebc9b776
fiorn <neros10v@yandex.ru>
2021-11-19 20:00:23 +0500

    Обновлено lesson5 задания 1 и 2 и 3

--- string_function.py
+++ string_function.py
@@ -1,8 +1,5 @@
 #задание 1
-print("Введите S1 и S2")
-S1=input()
-S2=input()
-def comare(S1,S2):
+def compare(S1,S2):
     ngrams = [
         S1[i:i+3] for i in range(len(S1))
     ]
@@ -10,4 +7,4 @@ def comare(S1,S2):
     for ngram in ngrams:
         count +=S2.count(ngram)
     return count/max(len(S1), len(S2))
-print(comare(S1,S2))
+
